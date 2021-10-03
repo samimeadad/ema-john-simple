2060759a5f53ca4c51eabcd23d089707a6e8b21c
Sami Meadad Choudhury <sami.meadad@gmail.com>
2021-10-03 18:01:55 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './App.css';
 import Header from './components/Header/Header';
 import Inventory from './components/Inventory/Inventory';
 import OrderReview from './components/OrderReview/OrderReview';
 import Shop from './components/Shop/Shop';
 import NotFound from './components/NotFound/NotFound';
+import './App.css';
 
 function App () {
   return (
